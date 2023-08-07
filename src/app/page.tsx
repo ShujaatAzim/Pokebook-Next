00c99bdb2af00f950f98f120325435237d401d29
Shujaat Azim <shujaat.azim@gmail.com>
2023-08-06 23:08:12 -0500

    refactor: :recycle: Begin skeleton.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main className=''>
       <div className=''>PokeBook</div>
+      <div>Navigation</div>
       <div>Landing Page</div>
     </main>
   );
